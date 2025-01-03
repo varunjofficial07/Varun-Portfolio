07f5ef248306d60a5d221f947355f4216c03da06
varunjofficial07 <126250606+varunjofficial07@users.noreply.github.com>
2025-01-03 20:33:39 +0530

    Update AboutCard.js

--- src/components/About/AboutCard.js
+++ src/components/About/AboutCard.js
@@ -33,7 +33,7 @@ function AboutCard() {
           <p style={{ color: "rgb(155 126 172)" }}>
             "Strive to build things that make a difference!"{" "}
           </p>
-          <footer className="blockquote-footer">Soumyajit</footer>
+          <footer className="blockquote-footer">Varun J</footer>
         </blockquote>
       </Card.Body>
     </Card>
